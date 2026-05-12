5bf1992a5c0514077a0e88859909761c0893026e
Dominik Krol <dominikkrol11@icloud.com>
2026-05-12 12:38:21 +0200

    dodanie hamburgera oraz funkcjonalnosc na telefony

--- script.js
+++ script.js
@@ -68,4 +68,27 @@ document.addEventListener('DOMContentLoaded', () => {
         sectionObserver.observe(section);
     });
 
+    // ==========================================
+    // 4. OBSŁUGA MENU MOBILNEGO (HAMBURGER)
+    // ==========================================
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.querySelector('.nav-links');
+    const navItems = document.querySelectorAll('.nav-links li a');
+
+    // Otwieranie/Zamykanie menu po kliknięciu w hamburger
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        hamburger.classList.toggle('toggle');
+    });
+
+    // Automatyczne zamykanie menu po kliknięciu w jakikolwiek link
+    navItems.forEach(item => {
+        item.addEventListener('click', () => {
+            if (navLinks.classList.contains('active')) {
+                navLinks.classList.remove('active');
+                hamburger.classList.remove('toggle');
+            }
+        });
+    });
+
 });
